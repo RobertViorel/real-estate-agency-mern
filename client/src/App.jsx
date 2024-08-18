917c8d5de0d0835239671727206efaaff4531e43
Robert051998 <96495255+Robert051998@users.noreply.github.com>
2024-08-18 16:39:35 +0200

    added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      <h1>Hello</h1>
+    <div className=''>
+      <BrowserRouter>
+      {/* Header will show on every page */}
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
